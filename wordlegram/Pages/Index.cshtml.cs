b4b8a3fc6fb8a0ac7679501ed74b8006b5f37c0c
bullfrog <sc4ld26@gmail.com>
2023-02-06 21:08:16 -0800

    new updates

--- wordlegram/Pages/Index.cshtml.cs
+++ wordlegram/Pages/Index.cshtml.cs
@@ -12,16 +12,91 @@ namespace wordlegram.Pages
         {
             _logger = logger;
         }
+        //global variables=
+        public bool showNext = false;
+        public void startGame()
+        {
+            HttpContext.Session.SetString("guess", "0");
+            string gameWord = wordPicker();
+            HttpContext.Session.SetString("gameWord", gameWord);
+            showNext = false;
+            for (int i = 0;i < 6; i++) 
+            { 
+                HttpContext.Session.SetString("game" + i, "");
+            }
+
+        }
+
+        public void endGame(bool victory, int guessPoints) 
+        //Handles the game ending in glorious victory or shameful defeat
+            {
+            if (victory)
+            {
+
+                //scorekeep increments the score upon correct guessing of word- we're passing in the guess number to get the correct score value
+                scoreKeep(guessPoints);
+                startGame();
+                showNext= true;
+
+            }
+            else
+            {
+
+            }     
+            }
+        public string wordPicker()
+        //Picks a word from a text file and 
+        {
+            string[] newWord = System.IO.File.ReadAllLines("Pages/wordlist.txt");
+            var rand = new Random();
+            long randomWord = rand.NextInt64(0, newWord.Length);
+            return newWord[randomWord];
+        } 
+
+        public void newWord()
+        {
+
+            HttpContext.Session.SetString("score", "0");
+            wordPicker();
+
+        }
+        public void scoreKeep(int pointsToGive)
+        {
+            //Handles scorekeeping 
+          int score = Convert.ToInt32(HttpContext.Session.GetString("score"));
+            switch (pointsToGive)
+            {
+                case 1:
+                    score += 1000;
+                    break;
+                case 2:
+                    score += 500;
+                    break;
+                case 3:
+                    score += 250;
+                    break;
+                case 4:
+                    score += 175;
+                    break;
+                case 5:
+                    score += 100;
+                    break;
+                case 6:
+                    score += 50;
+                    break;
+            }
+            HttpContext.Session.SetString("score", score.ToString());
+        }
 
         public string theGame(string guessWord)
         {
-        if (Convert.ToInt32(HttpContext.Session.GetString("guess")) >= 5)
+            //General Game 'engine'
+        if (Convert.ToInt32(HttpContext.Session.GetString("guess")) >= 6)
             {
-                return "You are out of guesses!";
+                return "You are out of tries!";
             }
-
+            string gameWord = HttpContext.Session.GetString("gameWord")!;
             int wordLength = 5;
-            string gameWord = "tacos";
             int guessNumber = Convert.ToInt32(HttpContext.Session.GetString("guess")) + 1;
             HttpContext.Session.SetString("guess", guessNumber.ToString());
             var gameResponse = new List<string>();
@@ -51,8 +126,14 @@ namespace wordlegram.Pages
 
                 if (gameWord.ToUpper() == guessWord.ToUpper())
                 {
+                endGame(true, guessNumber);
                 return htmlBlob;
                 } 
+                else if (guessNumber >= 6)
+                {
+                endGame(false, guessNumber);
+                return htmlBlob + "\nGame Over!";
+                }
                 else
                 {
                 return htmlBlob;
@@ -61,10 +142,12 @@ namespace wordlegram.Pages
         public void OnGet()
         {
             HttpContext.Session.SetString("guess", "0");
+            HttpContext.Session.SetString("score", "0");
+            startGame();
         }
         public void OnPost()
         {
-            
+
         }
     }
 }
--- wordlegram/Program.cs
+++ wordlegram/Program.cs
@@ -5,7 +5,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
 {
-    options.IdleTimeout = TimeSpan.FromSeconds(30);
+    options.IdleTimeout = TimeSpan.FromSeconds(300);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
 }
